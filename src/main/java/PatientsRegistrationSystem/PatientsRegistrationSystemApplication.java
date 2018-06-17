4a4f3c948bacee83f278ac66cb867172b7d02bc4
szymonides123 <szymek283@gmail.com>
2018-06-17 21:19:49 +0200

    first commit

---
+++ src/main/java/PatientsRegistrationSystem/PatientsRegistrationSystemApplication.java
@@ -0,0 +1,12 @@
+package PatientsRegistrationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsRegistrationSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/PatientsRegistrationSystem/PatientsRegistrationSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package PatientsRegistrationSystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PatientsRegistrationSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
