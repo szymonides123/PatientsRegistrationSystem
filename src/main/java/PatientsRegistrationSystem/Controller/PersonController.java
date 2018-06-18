b8fb65e52fa02ee0ab9b09f4d5c897857b041b0e
szymonides123 <szymek283@gmail.com>
2018-06-18 19:43:27 +0200

    created controllers and views

---
+++ src/main/java/PatientsRegistrationSystem/Controller/PersonController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PatientsRegistrationSystem.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PersonController {
+
+    @GetMapping("/person")
+    public String person() {
+        
+        return "person";
+    }
+
+}
---
+++ src/main/java/PatientsRegistrationSystem/Controller/RegisterController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PatientsRegistrationSystem.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RegisterController {
+    
+    public String register(){
+    
+        return "register";
+    }
+    
+}
