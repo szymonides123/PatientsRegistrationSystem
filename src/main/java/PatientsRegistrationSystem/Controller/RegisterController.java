683d19897ca76d40e6b4f39553d7a68951a0f906
szymonides123 <szymek283@gmail.com>
2018-06-18 22:14:02 +0200

    added thymeleaf

--- src/main/java/PatientsRegistrationSystem/Controller/RegisterController.java
+++ src/main/java/PatientsRegistrationSystem/Controller/RegisterController.java
@@ -6,10 +6,12 @@
 package PatientsRegistrationSystem.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RegisterController {
     
+    @GetMapping("/register")
     public String register(){
     
         return "register";
